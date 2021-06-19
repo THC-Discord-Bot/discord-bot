41af2d78df25c82980b9774b5261b94e1adf19d9
Ryan Wilson <tyanzemial@gmail.com>
2021-06-18 22:30:30 -0700

    Cleaned up original dsc.on then added a welcome command, kick, and ban

--- src/app.js
+++ src/app.js
@@ -17,33 +17,76 @@ const Hook = new webhook.Webhook(
 const dsc = new Discord.Client();
 dsc.login(config.BOT_TOKEN);
 
+dsc.on('ready', () => {
+  console.log('Bot has logged in')
+})
+
 // BOT COMMANDS PREFIX
-const prefix = "!tch";
+const prefix = "$";
 
-dsc.on("message", function (message) {
+// Event listener for when new guild members join
+dsc.on('guildMemberAdd', member => {
+  const channelId = '855280951293771778' // Welcome channel
+  const targetChannelId = '855576662162145310' // Rules and info
+
+  console.log(member)
+
+  const message = `Please welome <@${member.id}> to the server please check out ${member.guild.channels.cache.get(targetChannelId).toString()}`
+
+  const channel = member.guild.channels.cache.get(channel.id)
+  channel.send(message)
+});
+
+dsc.on("message", async (message) => {
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix)) {
+    const [CMD_NAME, ...args] = message.content
+    .trim()
+    .substring(prefix.length)
+    .split(/\s+/);
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(" ");
-  const command = args.shift().toLowerCase();
+    // KICK COMMAND
+    if (CMD_NAME === 'kick') {
+      // Checking if user has permissions
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You do not have permissions to use that command')
 
-  // CLEAR MESSAGES
-    if (args[0] == "clear") {
-      if (args[1] == undefined) {
-        message.channel.send("Usage: !atg clear <number of chats to clear>");  
+      // If user didnt give an id or user
+      if (args.length === 0) 
+        return message.reply('Please provide an ID');
+
+      // Member = the user that was @ or a user id to be kicked
+      const member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+
+      // If the member was found in the guild then the bot kicks them...
+      if (member) {
+        member
+        .kick()
+        .then((member) => message.channel.send(`${member} was kicked`))
+        .catch((err) => message.channel.send('I cannot kick that user :('))
       } else {
-        message.channel.bulkDelete(args[1])
-        .then(messages => console.log(`Bulk deleted ` + args[1] + ` messages`))
-        .catch(console.error);
-        message.channel.send("Chat cleared");    
-      }                    
+        message.channel.send('That member was not found')
+      }
     }
 
-  // HELP COMMAND
-  if (args[0] == "help") {
-    message.reply(
-      "Feature soon to be added"
-    );
+    // BAN COMMAND
+    if (CMD_NAME === 'ban') {
+      // Checking if user has permissions
+      if (!message.member.hasPermission('BAN_MEMBERS'))
+        return message.reply('You do not have permissions to use that command')
+      // Checking if user providing an ID
+      if (args.length === 0) return message.reply('Please provide an ID');
+
+      // Checking ID of user to be banned if not found then sends an error
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send('User was banned successfully')
+      } catch (err) {
+        console.log(err);
+        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
+      }
+    }
   }
 });
+
+dsc.login(process.env.BOT_TOKEN);
