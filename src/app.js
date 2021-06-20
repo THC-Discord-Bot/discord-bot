489ed9e4277c5611c294b5c62df912965108b931
HopelessTitan <amaankhalditheowl@gmail.com>
2021-06-20 06:55:33 +0530

    Project Clean up
    Cleaned up App.js code.
    
    Migrated Bot command functions to Commands folder.
    
    Migrated DiscordClient initialisation and login into app.js.
    
    Deleted unused files (main.ts, discordClient.js)
    
    Rewrote some code for readability.
    
    Removed unused variables and functions.

--- src/app.js
+++ src/app.js
@@ -1,111 +1,40 @@
-// REQUIRES
-const Discord = require("discord.js");
-const config = require("./config.json");
+const { Client } = require('discord.js');
+const config = require('./config.json');
+const { 
+  kickMember, 
+  BanUser, 
+  ClearMessages 
+} = require('./commands/general');
 
-// TWILIO SETUP
+// =========== Initialisations ===========
+const primaryPrefix = config.primaryPrefix;
+const client = new Client();
 
-// WEBHOOK SETUP
-const webhook = require("webhook-discord");
-const { resolve } = require("path");
-
-// Replace with your discord testing server webhook url
-const Hook = new webhook.Webhook(
-  "https://discordapp.com/api/webhooks/855281016574574672/7e5JRuhbN8URNvw1U1d3MjuqxT4NASaG0bVXbPtJdg5jZf4L0hIoeSyYxQCthriOQMZC"
-);
-
-// DISCORD SETUP
-const dsc = new Discord.Client();
-dsc.login(config.BOT_TOKEN);
-
-dsc.on('ready', () => {
-  console.log('Bot has logged in')
-})
-
-// BOT COMMANDS PREFIX
-const prefix = "$";
+// =========== Bot Initialisation ===========
+client.login(process.env.BOT_TOKEN);
+client.on('ready', () => {
+  console.log('Bot has logged in');
+});
 
 // Event listener for when new guild members join
-dsc.on('guildMemberAdd', member => {
-  const channelId = '855280951293771778' // Welcome channel
-  const targetChannelId = '855576662162145310' // Rules and info
-
-  console.log(member)
-
-  const message = `Please welome <@${member.id}> to the server please check out ${member.guild.channels.cache.get(targetChannelId).toString()}`
-
-  const channel = member.guild.channels.cache.get(channel.id)
-  channel.send(message)
+client.on('guildMemberAdd', (member) => {
+  const channel = member.guild.channels.cache.get(config.welcomeChannelId);
+  const targetChannelId = member.guild.channels.guild.cache.get('855576662162145310').toString(); // Rules and info
+  const message = `Please welome <@${member.id}> to the server please check out ${targetChannelId}`;
+  channel.send(message);
 });
 
-dsc.on("message", async (message) => {
+client.on('message', async (message) => {
   if (message.author.bot) return;
-  if (message.content.startsWith(prefix)) {
-    const [CMD_NAME, ...args] = message.content
-    .trim()
-    .substring(prefix.length)
-    .split(/\s+/);
-
-    // KICK COMMAND
-    if (CMD_NAME === 'kick') {
-      // Checking if user has permissions
-      if (!message.member.hasPermission('KICK_MEMBERS'))
-        return message.reply('You do not have permissions to use that command')
-
-      // If user didnt give an id or user
-      if (args.length === 0) 
-        return message.reply('Please provide an ID');
-
-      // Member = the user that was @ or a user id to be kicked
-      const member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-
-      // If the member was found in the guild then the bot kicks them...
-      if (member) {
-        member
-        .kick()
-        .then((member) => message.channel.send(`${member} was kicked`))
-        .catch((err) => message.channel.send('I cannot kick that user :('))
-      } else {
-        message.channel.send('That member was not found')
-      }
-    }
-
-    // BAN COMMAND
-    if (CMD_NAME === 'ban') {
-      // Checking if user has permissions
-      if (!message.member.hasPermission('BAN_MEMBERS'))
-        return message.reply('You do not have permissions to use that command')
-      // Checking if user providing an ID
-      if (args.length === 0) return message.reply('Please provide an ID');
-
-      // Checking ID of user to be banned if not found then sends an error
-      try {
-        const user = await message.guild.members.ban(args[0]);
-        message.channel.send('User was banned successfully')
-      } catch (err) {
-        console.log(err);
-        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
-      }
-    }
-
-    // CLEAR MESSAGES
-    if (CMD_NAME === "clear") {
-      if (args[1] == undefined) {
-        message.channel.send("Usage: !atg clear <number of chats to clear>");  
-      } else {
-        message.channel.bulkDelete(args[1])
-        .then(messages => console.log(`Bulk deleted ` + args[1] + ` messages`))
-        .catch(console.error);
-        message.channel.send("Chat cleared");    
-      }                    
-    }
-
-    // HELP COMMAND
-    if (CMD_NAME == "help") {
-      message.reply(
-        "Feature soon to be added"
-      )
-    }
+  if (message.content.startsWith(primaryPrefix+'kick ')){
+    kickMember(message);
   }
-});
+  if (message.content.startsWith(primaryPrefix+'ban ')){
+    BanUser(message);
+  }
+  if (message.content.startsWith(primaryPrefix+'clear ')){
+    ClearMessages(message);
+  }
 
-dsc.login(process.env.BOT_TOKEN);
+}
+);
--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,14 +1,14 @@
-module.exports = (commandArgs, message) => {
-  console.log("clear!!");
-  if (commandArgs[0] == undefined) {
-    message.channel.send("Usage: !tch clear <number of chats to clear>");
-  } else {
-    message.channel
-      .bulkDelete(commandArgs[0])
-      .then((messages) =>
-        console.log(`Bulk deleted ` + commandArgs[0] + ` messages`)
-      )
-      .catch(console.error);
-    message.channel.send("Chat cleared");
-  }
-};
+// module.exports = (commandArgs, message) => {
+//   console.log("clear!!");
+//   if (commandArgs[0] == undefined) {
+//     message.channel.send("Usage: !tch clear <number of chats to clear>");
+//   } else {
+//     message.channel
+//       .bulkDelete(commandArgs[0])
+//       .then((messages) =>
+//         console.log(`Bulk deleted ` + commandArgs[0] + ` messages`)
+//       )
+//       .catch(console.error);
+//     message.channel.send("Chat cleared");
+//   }
+// };
